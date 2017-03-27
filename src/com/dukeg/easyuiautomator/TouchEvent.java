69656f639980d1328564179bec1b0b2c5565ef91
DukeG <fireicewolf@gmail.com>
2017-03-27 12:04:43 +0800

    Change some touch event

--- src/com/dukeg/easyuiautomator/TouchEvent.java
+++ src/com/dukeg/easyuiautomator/TouchEvent.java
@@ -1,41 +1,20 @@
 package com.dukeg.easyuiautomator;
 
-import android.os.RemoteException;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.uiautomator.*;
-
-import java.lang.reflect.InvocationTargetException;
 
 public class TouchEvent {
     private UiDevice mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
 
     //Click once action in pixels
     public boolean click(int x, int y) {
-       if (mDevice.getDisplayWidth() / mDevice.getDisplayHeight() == 9/16) {
-           return mDevice.click(x * mDevice.getDisplayWidth() / 1080, y * mDevice.getDisplayHeight() / 1920);
-       }
-       if (mDevice.getDisplayWidth() / mDevice.getDisplayHeight() == 16/9) {
-           return mDevice.click(x * mDevice.getDisplayWidth() / 1920, y * mDevice.getDisplayHeight() / 1080);
-       }
-       else {
-           return mDevice.click(x ,y );
-       }
+       return mDevice.click(x ,y );
     }
 
     //Long Click action in pixels
     public boolean longClick(int x, int y, int pressTime) {
         int steps = pressTime / 5;
-        if (mDevice.getDisplayWidth() / mDevice.getDisplayHeight() == 9/16) {
-            return mDevice.swipe(x * mDevice.getDisplayWidth() / 1080, y * mDevice.getDisplayHeight() / 1920,
-                    x * mDevice.getDisplayWidth() / 1080, y * mDevice.getDisplayHeight() / 1920, steps);
-        }
-        if (mDevice.getDisplayWidth() / mDevice.getDisplayHeight() == 16/9) {
-            return mDevice.swipe(x * mDevice.getDisplayWidth() / 1920, y * mDevice.getDisplayHeight() / 1080,
-                    x * mDevice.getDisplayWidth() / 1920, y * mDevice.getDisplayHeight() / 1080, steps);
-        }
-        else {
-            return mDevice.swipe(x, y ,x ,y , steps);
-        }
+        return mDevice.swipe(x, y ,x ,y , steps);
     }
 
     //Multi Click action in pixels
@@ -43,15 +22,7 @@ public class TouchEvent {
         long timeout = Configurator.getInstance().getActionAcknowledgmentTimeout();
         Configurator.getInstance().setActionAcknowledgmentTimeout(interval);
         for (int i = 0; i < times; i++) {
-            if (mDevice.getDisplayWidth() / mDevice.getDisplayHeight() == 9/16) {
-                mDevice.click(x * mDevice.getDisplayWidth() / 1080, y * mDevice.getDisplayHeight() / 1920);
-            }
-            if (mDevice.getDisplayWidth() / mDevice.getDisplayHeight() == 16/9) {
-                mDevice.click(x * mDevice.getDisplayWidth() / 1920, y * mDevice.getDisplayHeight() / 1080);
-            }
-            else {
-                mDevice.click(x, y);
-            }
+            mDevice.click(x, y);
         }
         Configurator.getInstance().setActionAcknowledgmentTimeout(timeout);
     }
@@ -83,22 +54,26 @@ public class TouchEvent {
 
     //Scroll up screen
     public boolean scrollUp() {
-        return mDevice.swipe(540 * mDevice.getDisplayWidth() / 1080, 1440 * mDevice.getDisplayHeight() / 1920, 540 * mDevice.getDisplayWidth() / 1080, 480 * mDevice.getDisplayHeight() / 1920, 100);
+        return mDevice.swipe(mDevice.getDisplayWidth() / 2,mDevice.getDisplayHeight() * 3 / 4,
+                mDevice.getDisplayWidth() / 2, mDevice.getDisplayHeight() / 4, 100);
     }
 
     //Scroll down screen
     public boolean scrollDown() {
-        return mDevice.swipe(540 * mDevice.getDisplayWidth() / 1080, 480 * mDevice.getDisplayHeight() / 1920, 540 * mDevice.getDisplayWidth() / 1080, 1440 * mDevice.getDisplayHeight() / 1920, 100);
+        return mDevice.swipe(mDevice.getDisplayWidth() / 2, mDevice.getDisplayHeight() / 4,
+                mDevice.getDisplayWidth() / 2, mDevice.getDisplayHeight() * 3 / 4, 100);
     }
 
     //Scroll left screen
     public boolean scrollLeft() {
-        return mDevice.swipe(810 * mDevice.getDisplayWidth() / 1080, 960 * mDevice.getDisplayHeight() / 1920, 270 * mDevice.getDisplayWidth() / 1080, 960 * mDevice.getDisplayHeight() / 1920, 100);
+        return mDevice.swipe(mDevice.getDisplayWidth() * 3 / 4, mDevice.getDisplayHeight() / 2,
+                mDevice.getDisplayWidth() * 1 / 4, mDevice.getDisplayHeight() / 2, 100);
     }
 
     //Scroll right screen
     public boolean scrollRight() {
-        return mDevice.swipe(270 * mDevice.getDisplayWidth() / 1080, 960 * mDevice.getDisplayHeight() / 1920, 810 * mDevice.getDisplayWidth() / 1080, 960 * mDevice.getDisplayHeight() / 1920, 100);
+        return mDevice.swipe(mDevice.getDisplayWidth() / 4, mDevice.getDisplayHeight() / 2,
+                mDevice.getDisplayWidth() * 3 / 4, mDevice.getDisplayHeight() / 2, 100);
     }
 
 }
